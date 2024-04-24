30428317ba4ae5cbe447cee2d172f4bc3403e6fd
Noah Walker <nwalker7727@gmail.com>
2024-04-23 22:55:21 -0700

    Updates for lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,7 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock, Paper, Scissors
+//An implementation of the game
+// Author: Noah Walker
+// Date: April 22, 2024
 
 // Constants
 
@@ -14,8 +15,59 @@ function myFunction(param1, param2) {
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+  /*
+    Pseudocode for creating a simple game like Rock Paper Scissors
+    1. print new board
+    2. accept user input
+    3. validate user input
+    4. randomize computer response
+    5. check win condition or draw condition
+    6. add 1 point to winner or 0 for tie
+    7. Repeat
+
+    Pseudocode for Rock, Paper, Scissors - fleshed out
+    print rock, paper, scissor, shoot
+    accept player input
+    check player input against options
+    if rock
+      randomize computer input
+      check computer input against player input
+        if paper
+          add 1 point to computer
+        else if scissors
+          add 1 point to player
+        else
+          no points added, reset board
+    if paper
+      randomize computer input
+      check computer input against player input
+        if scissors
+          add 1 point to computer
+        else if rock
+          add 1 point to player
+        else
+          no points added, reset board
+    if scissors
+      randomize computer input
+      check computer input against player input
+        if rock
+          add 1 point to computer
+        else if paper
+          add 1 point to player
+        else
+          no points added
+    print score
+    print newline at the end
+    print a horizontal line between
+      
+
+
+
+
+*/
 }
 
 // let's get this party started
-main();
+main(){
+
+};

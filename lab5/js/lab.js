a0ed83e7197297a0b3b14e7baa59085970441365
Noah Walker <nwalker7727@gmail.com>
2024-04-25 15:56:30 -0700

    updates for lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
+/* 
+* Author: Noah Walker <newalker@ucsc.edu>
+* Created: April 25, 2024
+* License: Public Domain
+*/
 // Constants
-
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
 // Functions
+age = 2019 - year;
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -13,8 +18,11 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model+ "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + "years<br>");
 }
 
 // let's get this party started

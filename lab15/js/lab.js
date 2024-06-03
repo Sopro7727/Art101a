26a8d858fee0d00b966ad2e35e0797f36b085f75
Noah Walker <nwalker7727@gmail.com>
2024-06-02 18:26:01 -0700

    Script added lab13 + finished lab15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -2,20 +2,54 @@
 // Author: Your Name
 // Date:
 
-// Constants
+const URL = "https://perenual.com/api/pest-disease-list";
 
-// Functions
+$("#activate").click(function(){
+  console.log("Click");
+  // call ajax
+  $.ajax(ajaxObj);
+})
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+const ajaxObj = {
+  url: URL,
+  data: {
+    key: "sk-uCEA6659018900ac35690",
+  },
+  type: "GET",
+  dataType: "json",
+  success: ajaxSuccess,
+  error: ajaxError
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+function ajaxSuccess(data) {
+  const dataArray = data.data;
+  console.log(dataArray.length)
+  const randomIndex = Math.floor(Math.random() * dataArray.length);
+  console.log(randomIndex)
+  const element1 = dataArray[randomIndex];
+  console.log("element1:", element1);
+  const question1 = element1.description[0].subtitle;
+  const answer1 = element1.description[0].description;
+  const question2 = element1.description[1].subtitle;
+  const answer2 = element1.description[1].description;
+  let imageURL = null;
+  try{
+    imageURL = element1.images[0].regular_url;
+  }
+  catch(error){
+    $.ajax(ajaxObj);
+  }
+  $("#output").html("<h3>" + question1);
+  if(imageURL != null){
+     $("#output").append("<img src='" + imageURL + "' />")
+  }
+  $("#output").append("<p>" + answer1);
+  $("#output").append("<h4>" + question2);
+  $("#output").append("<p>" + answer2);
 }
 
-// let's get this party started
-main();
+
+// create ajax error callback
+function ajaxError(request,error){
+	  console.log("Oops:", error);
+}
